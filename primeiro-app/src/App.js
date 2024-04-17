51cf310427c3137d05c948c96416ddaca35801b3
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-04-17 20:51:40 -0300

    Consumindo API

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,49 +1,42 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import './style.css';
+
+// API URL
+// https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
-  const [input, setInput] = useState("");
-  const [tarefas, setTarefas] = useState([]);
+const [nutri,setNutri] = useState([]);
 
-  // Se deixar o array vazio toda vez que recarregar a pagina essa função será executada
-  // se passar um parametro dentro do Array [] toda vez que sofrer uma alteração a função é executada
-  useEffect(() =>{
-    localStorage.setItem('@tarefa', JSON.stringify(tarefas))
-  }, [tarefas])
+useEffect(() => {
+  function loadingAPI(){
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
 
-
-  // e.preventDefault(); Serve para o navegador não ficar atualizando infinitamente
-  // setTarefas([...tarefas,input]); os 3 pontos está incrementando o valor do Array tarefas + input digitado
-  // setInput(''); está limpando campo input
-  function handleRegister(e) {
-    e.preventDefault();
-    setTarefas([...tarefas, input]);
-    setInput("");
+    fetch(url)
+    .then((resultado) => resultado.json())
+    .then((json=> {
+      setNutri(json)
+    }))
   }
-
-  // <li key={tarefa}>{tarefa}</li>  - Todas listas em React precisa ter uma variavel key
+  loadingAPI()
+})
   return (
-    <div>
-      <h1>Cadastro de usuários</h1>
-      <form onSubmit={handleRegister}>
-        <label>Tarefa:</label>
-        <input
-          placeholder="Digite uma tarefa"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <br />
-        <br />
-        <button type="submit">Registrar</button>
-      </form>
-      <br />
-      <br />
-      <div>
-        <ul>
-          {tarefas.map((tarefa) => (
-            <li key={tarefa}>{tarefa}</li>
-          ))}
-        </ul>
-      </div>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item) => {
+        return(
+          <article key={item.id} className="post">
+            <strong className="titulo">{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className="capa" />
+            <p className="subtitulo">
+              {item.subtitulo}
+            </p>
+            <a className="botao">Acessar</a>
+          </article>
+        )
+      } )}
     </div>
   );
 }
