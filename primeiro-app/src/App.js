14e42d0fe41fdc18fc1e6ede5c9c6b7a3e855e91
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-04-17 19:58:55 -0300

    Trabalhando com listas em react

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,68 +1,40 @@
 import { useState } from "react";
 
 function App() {
-  //useState nome,email,idade
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [idade, setIdade] = useState("");
-
-  //useState que armazena todas propriedades nome,email,idade, sendo declarado como um objeto vazio
-  // Dentro dos parenteses é passado o valor por default fixo ao criar useState({});
-  const [user, setUser] = useState({});
-
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+  // e.preventDefault(); Serve para o navegador não ficar atualizando infinitamente
+  // setTarefas([...tarefas,input]); os 3 pontos está incrementando o valor do Array tarefas + input digitado
+  // setInput(''); está limpando campo input
   function handleRegister(e) {
     e.preventDefault();
-    // aqui é passado o valor de user
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email,
-    });
+    setTarefas([...tarefas, input]);
+    setInput("");
   }
 
-  // Formulario com evento onSubmit chamando a função handleRegister
-  // Essa propriedade pega o valor do input e atribuit a setNome onChange={(e) => setNome(e.target.value)
-  // Assim está sendo acessado a propriedade nome do objeto user definido acima <span>Bem vindo: {user.nome} </span> <br/><br/>
+  // <li key={tarefa}>{tarefa}</li>  - Todas listas em React precisa ter uma variavel key
   return (
     <div>
       <h1>Cadastro de usuários</h1>
       <form onSubmit={handleRegister}>
-        <label>Nome:</label>
+        <label>Tarefa:</label>
         <input
-          placeholder="Digite seu nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />{" "}
-        <br />
-        <br />
-        <label>Email:</label>
-        <input
-          placeholder="Digite seu email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />{" "}
-        <br />
-        <br />
-        <label>Idade:</label>
-        <input
-          placeholder="Digite sua idade"
-          value={idade}
-          onChange={(e) => setIdade(e.target.value)}
-        />{" "}
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <br />
         <br />
         <button type="submit">Registrar</button>
       </form>
       <br />
       <br />
-
       <div>
-        <span>Bem vindo: {user.nome} </span> <br />
-        <br />
-        <span>Idade: {user.idade} </span> <br />
-        <br />
-        <span>Email: {user.email} </span> <br />
-        <br />
+        <ul>
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
