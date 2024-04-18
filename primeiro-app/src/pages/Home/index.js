2f7106cf780e5f2c08410e1b03b7f497295a26e6
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-04-17 21:23:06 -0300

    Trabalhando com Links

--- primeiro-app/src/pages/Home/index.js
+++ primeiro-app/src/pages/Home/index.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div>
       <h1>Bem vindo a pagina inicial</h1>
+
+      <Link to="/sobre">Sobre</Link>
     </div>
   );
 }
--- primeiro-app/src/pages/Sobre/index.js
+++ primeiro-app/src/pages/Sobre/index.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 function Sobre() {
     return (
       <div>
         <h1>Bem vindo a pagina Sobre</h1>
+        <Link to="/">Home</Link>
       </div>
     );
   }
